1337390379ae68ec3fbb07f543582a6b6094d71b
G0ONar <142931976+G0ONar@users.noreply.github.com>
2025-12-05 22:01:22 -0800

    fadafds

---
+++ Cards.java
@@ -0,0 +1,43 @@
+//This creates card objects
+public class Cards {
+    private final int rank;
+    private final int suit;
+
+    public Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rankToString(rank) + " of " + suitToString(suit);
+    }
+//Case means switch
+    private String rankToString(int r) {
+        switch (r) {
+            case 11: return "Jack";
+            case 12: return "Queen";
+            case 13: return "King";
+            case 14: return "Ace";
+            default: return String.valueOf(r);
+        }
+    }
+
+    private String suitToString(int s) {
+        switch (s) {
+            case 0: return "Clubs";
+            case 1: return "Diamonds";
+            case 2: return "Hearts";
+            case 3: return "Spades";
+            default: return "Unknown";
+        }
+    }
+}
---
+++ Suits.java
@@ -0,0 +1,7 @@
+//All classes of cards
+public class Suits {
+    public static final int CLUBS = 0;
+    public static final int DIAMONDS = 1;
+    public static final int HEARTS = 2;
+    public static final int SPADES = 3;
+}
